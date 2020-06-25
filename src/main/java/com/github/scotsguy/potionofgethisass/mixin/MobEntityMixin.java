cc6e0212be95d0cff2e4cd2e27f9a3a422a09761
AppleTheGolden <scotsbox@protonmail.com>
2020-06-25 14:21:48 +0200

    Passive Entities will attack you too now Bump to v1.1.0

--- src/main/java/com/github/scotsguy/potionofgethisass/mixin/MobEntityMixin.java
+++ src/main/java/com/github/scotsguy/potionofgethisass/mixin/MobEntityMixin.java
@@ -3,10 +3,12 @@ package com.github.scotsguy.potionofgethisass.mixin;
 import com.github.scotsguy.potionofgethisass.GetHisAssGoal;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.GoalSelector;
-import net.minecraft.entity.mob.Angerable;
-import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.MobEntityWithAi;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,9 +16,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-// Adapted from https://gitea.thebrokenrail.com/TheBrokenRail/Twine/src/branch/master/src/main/java/com/thebrokenrail/twine/mixin/MixinMobEntity.java
-// by TheBrokenRail, MIT licensed.
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @SuppressWarnings("ConstantConditions")
 @Mixin(MobEntity.class)
@@ -25,16 +25,24 @@ public class MobEntityMixin {
     @Final
     protected GoalSelector targetSelector;
 
+    @Shadow
+    @Final
+    protected GoalSelector goalSelector;
+
     @Inject(at = @At("RETURN"), method = "<init>")
     public void initGoals(EntityType<? extends MobEntity> entityType, World world, CallbackInfo info) {
         if (world != null && !world.isClient()) {
-            if (
-                    (Object) this instanceof HostileEntity
-                            || (Object) this instanceof MobEntityWithAi
-                            || this instanceof Angerable)
-            {
+            if ((Object) this instanceof MobEntityWithAi) {
                 targetSelector.add(2, new GetHisAssGoal((MobEntity) (Object) this));
             }
+            if ((Object) this instanceof PassiveEntity) {
+                this.goalSelector.add(1, new MeleeAttackGoal((MobEntityWithAi) (Object) this, 1.125, false));
+            }
         }
+    }
+
+    @Inject(method = "createMobAttributes", at = @At(value = "RETURN"))
+    private static void allEnemiesCanAttack(CallbackInfoReturnable<DefaultAttributeContainer.Builder> ci) {
+        ci.getReturnValue().add(EntityAttributes.GENERIC_ATTACK_DAMAGE);
     }
 }
